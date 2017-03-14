2b939891be001f688878d58b9053bd0b91dc0d79
imadcn <nevase@foxmail.com>
2017-03-14 11:43:03 +0800

    update mq tester

--- imadcn-test/src/main/java/com/imadcn/test/mq/MqListenerTest.java
+++ imadcn-test/src/main/java/com/imadcn/test/mq/MqListenerTest.java
@@ -2,18 +2,14 @@ package com.imadcn.test.mq;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.imadcn.demo.rabbitmq.Sender;
-
 public class MqListenerTest {
 	
 	private ClassPathXmlApplicationContext context;
-	private String configPath = "classpath:spring-config.xml";
-	protected Sender sender;
+	private String configPath = "classpath:spring-config-mq-listener.xml";
 	
 	public MqListenerTest() {
 		context = new ClassPathXmlApplicationContext(new String[] { configPath });
 		context.start();
-		sender = context.getBean(Sender.class);
 	}
 	
 	public static void main(String[] args) throws Exception {
